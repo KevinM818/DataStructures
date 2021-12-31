c08e46fd36d8702bef3b25e781424ee0b26b7a02
Kevin Mangal <kmangal818@gmail.com>
2021-12-31 01:45:39 -0500

    first commit

---
+++ SinglyLinkedList.js
@@ -0,0 +1,170 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+
+    let current = this.head;
+    let newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return current;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+
+    const currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+
+    if (this.length === 0) {
+      this.tail = null;
+    }
+
+    return currentHead;
+  }
+
+  unshift(val) {
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+    }
+
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+
+    let counter = 0;
+    let current = this.head;
+
+    while(counter !== index) {
+      current = current.next;
+      counter++;
+    }
+
+    return current;
+  }
+
+  set(index, val) {
+    const foundNode = this.get(index);
+    
+    if (foundNode) {
+      foundNode.val = val;
+      return true
+    }
+
+    return false;
+  }
+
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return !!this.push(val);
+    if (index === 0) return !!this.unshift(val);
+
+    const newNode = new Node(val);
+    const prev = this.get(index - 1);
+    const temp = prev.next;
+
+    prev.next = newNode;
+    newNode.next = temp;
+    this.length++;
+    
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index > this.length) return undefined;
+    if (index === this.length - 1) return this.pop();
+    if (index === 0) return this.shift();
+
+    const previousNode = this.get(index - 1);
+    const removed = previousNode.next;
+
+    previousNode.next = removed.next;
+    this.length--;
+    
+    return removed;
+  }
+
+  reverse() {
+    let node = this.head;
+    let prev = null;
+    let next;
+
+    this.head = this.tail;
+    this.tail = node;
+
+    while (node) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    
+    return this;
+  }
+
+  traverse() {
+    let temp = this.head;
+
+    while(temp) {
+      console.log(temp.val);
+      temp = temp.next;
+    }
+  }
+
+}
+
+const list = new SinglyLinkedList();
+list.push('ONE');
+list.push('TWO');
+list.push('THREE');
+list.push('FOUR');
+list.push('FIVE');
